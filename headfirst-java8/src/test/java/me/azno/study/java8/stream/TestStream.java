67454e360a32f4151a533d53e70a2f96a66bf01b
yuleima <mayl@zetyun.com>
2018-03-27 10:51:58 +0800

    0327

--- headfirst-java8/src/test/java/me/azno/study/java8/stream/TestStream.java
+++ headfirst-java8/src/test/java/me/azno/study/java8/stream/TestStream.java
@@ -8,6 +8,6 @@ public class TestStream {
     @Test
     public void test() {
         int[] array = new int[]{1, 2, 3, 4};
-        IntStream.of(array).map(i -> i > 2 ? i : 2).forEach(i -> System.out.println(i));
+        IntStream.of(array).map(i -> i > 2 ? i : 2).forEach(System.out::println);
     }
 }
