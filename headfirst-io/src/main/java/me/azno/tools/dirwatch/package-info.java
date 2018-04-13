8fe78b7f900ebc262e865fd9eeaa20c0a69486b9
yuleima <17914393@qq.com>
2018-04-14 01:03:44 +0800

    0414 kickoff dirwatch

---
+++ headfirst-io/src/main/java/me/azno/tools/dirwatch/package-info.java
@@ -0,0 +1,1 @@
+package me.azno.tools.dirwatch;
---
+++ headfirst-io/src/main/java/me/azno/tools/dirwatch/tmp/T1.java
@@ -0,0 +1,92 @@
+package me.azno.tools.dirwatch.tmp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class T1 {
+    private static final Logger logger = LoggerFactory.getLogger("test");
+
+    public static void main(String[] args) throws Exception {
+        String path = "C:\\Users\\leigh";
+        path = "C:\\Users\\leigh\\AppData\\Local\\Microsoft\\Windows\\INetCache\\Low";
+        path = "C:\\Users\\leigh\\AppData\\Local\\Microsoft\\Windows";
+        File root = new File(path);
+//        list(root);
+//        listFiles(root);
+//        space(root);
+        Map<String, File> map = new HashMap<>();
+//        recursion(root, map);
+        recursion2(root, map);
+        logger.info("{}", map.size());
+//        for (Map.Entry<String, File> entry : map.entrySet()) {
+//            logger.info("{}", entry.getKey());
+//        }
+        long fileCount = map.values().stream().filter(file -> file.isFile()).count();
+        long dirCount = map.values().stream().filter(file -> file.isDirectory()).count();
+        logger.info("{} {} ", fileCount, dirCount);
+    }
+
+    private static void space(File file) {
+        logger.info("TotalSpace:{} , UsableSpace:{}", file.getTotalSpace(), file.getUsableSpace());
+    }
+
+    private static void list(File file) {
+        for (String fileName : file.list()) {
+            logger.info("{}", fileName);
+        }
+    }
+
+    private static void listFiles(File path) {
+        for (File file : path.listFiles()) {
+            logger.info(
+                    "{} {} {} {}",
+                    file.getName(),
+                    file.isFile() ? "file" : "-",
+                    file.isDirectory() ? "dir" : "-",
+                    file.isHidden() ? "hidden" : "-"
+            );
+        }
+
+    }
+
+    private static void recursion(File path, Map<String, File> map) throws Exception {
+        if (map == null)
+            throw new Exception("map is null");
+//        map.put(path.getName(), path);
+        if (path.isDirectory()) {
+            try {
+                File[] files = path.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        map.put(file.getAbsolutePath(), file);
+                        if (file.isDirectory())
+                            recursion(file, map);
+                    }
+                }
+            } catch (Throwable e) {
+                logger.error("path {} ", path);
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    private static void recursion2(File path, Map<String, File> map) throws Exception {
+        if (map == null)
+            throw new Exception("map is null");
+        File[] files = path.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                map.put(file.getAbsolutePath(), file);
+                if (file.isDirectory())
+                    recursion2(file, map);
+            }
+        }
+    }
+
+}
